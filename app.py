bb342b670939e4b98b494e1e5fad1b7eb075cebd
AnkurAlpha <ankuralpha404@gmail.com>
2025-05-18 22:45:27 +0530

    final debug and designing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)  # just to refernce the file
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 # triple slash for relative path # quadriple slash for absolute
 
 # need to initialize database
@@ -13,8 +13,6 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # first column for ID
     content = db.Column(db.String(200), nullable=False)  # second column
-    completed = db.Column(db.Integer, default=0)
-    # status of the task completion
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     # date of creation
 
@@ -48,6 +46,7 @@ def index():
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
+
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
@@ -68,7 +67,6 @@ def update(id):
             return "There was an issue updating your task"
     else:
         return render_template('update.html', task=task)
-    return ''
 
 
 if __name__ == "__main__":
